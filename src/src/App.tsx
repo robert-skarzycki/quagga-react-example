34a37d8179c59a474f7bfad0331ae1e4c4f42dc5
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2020-04-25 19:04:14 +0200

    Quagge used in React

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Quagga from "quagga";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuaggaElement />
     </div>
   );
 }
+
+const QuaggaElement = () => {
+  const [code, setCode] = useState();
+  const onDetected = (data: any) => {
+    setCode(data.codeResult.code);
+  };
+
+  useEffect(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          type: "LiveStream",
+          constraints: {
+            width: 640,
+            height: 480,
+            facing: "environment", // or user
+          },
+        },
+        locator: {
+          patchSize: "medium",
+          halfSample: true,
+        },
+        numOfWorkers: 2,
+        decoder: {
+          readers: ["ean_reader"],
+          debug: {
+            drawBoundingBox: false,
+            showFrequency: false,
+            drawScanline: false,
+            showPattern: false,
+          },
+          multiple: false,
+        },
+        locate: true,
+      },
+      function (err: any) {
+        if (err) {
+          return console.log(err);
+        }
+        Quagga.start();
+      }
+    );
+    Quagga.onDetected(onDetected);
+
+    return () => {
+      Quagga.offDetected(onDetected);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>{code}</h1>
+      <div id="interactive" className="viewport" />
+    </div>
+  );
+};
 
 export default App;
